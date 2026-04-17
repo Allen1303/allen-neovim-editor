401cae328024789d86c439b7c78ad6d190e4a38a
Allen <132642677+Allen1303@users.noreply.github.com>
2026-04-17 16:30:33 -0500

    Update react snippets file and json file for better support

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -135,8 +135,8 @@ return {
 
 				-- ── Sources ───────────────────────────────────────────────
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp", priority = 1000 },
-					{ name = "luasnip", priority = 750 },
+					{ name = "luasnip", priority = 1000 },
+					{ name = "nvim_lsp", priority = 750 },
 					{ name = "path", priority = 500 },
 					{ name = "buffer", priority = 250 },
 				}),
